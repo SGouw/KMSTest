e865acf9d28c3cfdd9a22be960658dbfec31e96d
Stephanie Gouw <s.gouw@student.unsw.edu.au>
2019-06-06 02:56:41 +1000

    yay the message works

--- app/src/main/java/com/example/kmstest/DisplayMessageActivity.java
+++ app/src/main/java/com/example/kmstest/DisplayMessageActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,21 @@ public class DisplayMessageActivity extends AppCompatActivity {
         //Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openDialog();
+            }
+        });
+
     }
+
+    public void openDialog(){
+        ExampleDialog edia = new ExampleDialog();
+        edia.show(getSupportFragmentManager(), "example dialog");
+    }
+
+
 }
